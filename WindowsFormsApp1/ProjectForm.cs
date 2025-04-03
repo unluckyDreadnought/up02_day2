ccb365f41356ca04b5eb4510aa7392788add383d
student251 <33334@gmail.com>
2025-04-03 11:45:29 +0300

    CLOSED #8 page changing by click made

--- WindowsFormsApp1/ProjectForm.cs
+++ WindowsFormsApp1/ProjectForm.cs
@@ -20,19 +20,23 @@ namespace WindowsFormsApp1
         private void UpdatePages(int pagesCount, int curPage)
         {
             panel1.Controls.Clear();
-            for (int n = pagesCount-1; n >= 0; n--)
+            int n = pagesCount - 1;
+            do
             {
                 Label page = new Label();
                 page.AutoSize = true;
-                page.Text = $"{n + 1}";
+                page.Text = (n  >= 0) ? $"{n + 1}" : $"{n+2}";
                 page.Dock = DockStyle.Left;
+                page.Click += page_Clicked;
                 if (page.Text == curPage.ToString())
                 {
                     page.BackColor = Color.CadetBlue;
                     page.ForeColor = Color.White;
                 }
                 panel1.Controls.Add(page);
+                n--;
             }
+            while (n >= 0);
         }
 
         private void UpdateTable(int offset = 1) {
@@ -60,7 +64,12 @@ namespace WindowsFormsApp1
             UpdateTable();
         }
 
-
+        private void page_Clicked(object sender, EventArgs e)
+        {
+            int page = 1;
+            int.TryParse(((Label)sender).Text, out page);
+            UpdateTable(page);
+        }
 
     }
 }
